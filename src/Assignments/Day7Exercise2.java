9feefe0a3fa1a37963d8267370fc0b194397390e
Brandon Carrasco <brancarrasco@outlook.com>
2021-10-26 17:24:30 -0400

    Day7 exercise 2

---
+++ src/Assignments/Day7Exercise2.java
@@ -0,0 +1,21 @@
+package Assignments;
+
+import java.util.Scanner;
+
+public class Day7Exercise2
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Give me your name, roll number, and field of interest");
+        String name = sc.nextLine();
+        int rollNum = sc.nextInt();
+        sc.nextLine();
+        String fieldOfInterest = sc.nextLine();
+
+        System.out.println("Hey, my name is " + name + " and my roll number is " +
+                rollNum + ". My field of interest is " + fieldOfInterest + ".");
+
+    }
+}
